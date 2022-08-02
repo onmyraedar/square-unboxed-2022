2cd64f39a40453957fc30ca7edbc58bb0a333bac
Reanna Ishmael <rishmael@fordham.edu>
2022-08-02 17:11:08 -0400

    Connect Express backend with React app
    Add a proxy key in the package.json file in the client folder.
    
    This will allow the React app to proxy requests to the Express app.
    
    Also, write a getMessage function to test the connection between the backend and frontend.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  async function getMessage() {
+    try {
+      const response = await fetch("/message");
+      const messageData = await response.json();
+      setMessage(messageData);
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getMessage}>Get Message</button>
+      <p>{message}</p>
     </div>
   );
 }
--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-    return res.send("Hello world!");
+    return res.json("Hello world!");
+});
+
+app.get("/message", (req, res) => {
+    return res.json("Don't have a good day, have a great day.");
 });
 
 const port = process.env.PORT || 5000;
