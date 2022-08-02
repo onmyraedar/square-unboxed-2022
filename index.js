bf7bcbc9784031a6db88850bd6975d44715ee577
Reanna Ishmael <rishmael@fordham.edu>
2022-08-02 17:40:03 -0400

    Add code necessary for deployment
    Tell Express to serve static files from the React app.
    
    Add a postbuild script to start the React app's production build.

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ const app = express();
 
 app.use(cors());
 
+// Get static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+
 app.get("/", (req, res) => {
     return res.json("Hello world!");
 });
 
 app.get("/message", (req, res) => {
     return res.json("Don't have a good day, have a great day.");
+});
+
+// Send back index.html for any request
+// that does not match the above
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 const port = process.env.PORT || 5000;
