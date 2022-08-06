d205601992b0619f36a30e3e20f9ddc9ebe417f9
Reanna Ishmael <rishmael@fordham.edu>
2022-08-06 11:58:37 -0400

    Add schema for database documents
    Two schema have been added: one for catalog item variations, and one for inventory items.
    
    The catalog item variations come from items that the seller has available for sale in Square.

---
+++ models/catalogitemvariation.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const CatalogItemVariationSchema = new Schema({
+    catalog_object_id: { type: String, required: true },
+    name: { type: String, required: true },
+    recipe: [{
+        ingredient: { type: Schema.Types.ObjectId, ref: "InventoryItem" },
+        quantity: { type: Schema.Types.Decimal128, default: 0 }
+    }],
+    modifiers: [{
+        catalog_object_id: { type: String, required: true },
+        ingredient: { type: Schema.Types.ObjectId, ref: "InventoryItem" },
+        quantity: { type: Schema.Types.Decimal128, default: 0 }
+    }]
+})
+
+module.exports = mongoose.model("CatalogItemVariation", CatalogItemVariationSchema);
---
+++ models/inventoryitem.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const InventoryItemSchema = new Schema({
+    name: { type: String, required: true },
+    quantity_in_stock: { type: Schema.Types.Decimal128, required: true, default: 0 },
+    unit: {
+        single: { type: String, required: true },
+        plural: { type: String, required: true }
+    }
+});
+
+module.exports = mongoose.model("InventoryItem", InventoryItemSchema);
