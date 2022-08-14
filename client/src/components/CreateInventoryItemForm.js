8a07a237348d66c3fc1d91f45dfbc88c0754a0bd
Reanna Ishmael <rishmael@fordham.edu>
2022-08-13 21:11:33 -0400

    Add form to the CreateInventoryItem component
    When the form is submitted, a new inventory item is created in the database.

--- client/src/components/CreateInventoryItemForm.js
+++ client/src/components/CreateInventoryItemForm.js
@@ -1,42 +1,109 @@
 import React, { useState } from "react";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { useNavigate } from "react-router-dom";
 
 function CreateInventoryItemForm() {
-  const [data, setData] = useState("No data yet");
+  const [inventoryItem, setInventoryItem] = useState({
+    name: "",
+    quantity_in_stock: 0,
+    singular_unit: "",
+    plural_unit: "",
+  });
 
-  // Testing a POST request
-  async function addInventoryItem() {
+  let navigate = useNavigate();
+
+  async function createInventoryItem(event) {
+    event.preventDefault();
     console.log("Initializing inventory item...");
-    const inventoryItem = {
-      name: "Ice Cream",
-      quantity_in_stock: 100,
+    const inventoryItemData = {
+      name: inventoryItem.name,
+      quantity_in_stock: inventoryItem.quantity_in_stock,
       unit: {
-        singular: "ounce",
-        plural: "ounces"
+        singular: inventoryItem.singular_unit,
+        plural: inventoryItem.plural_unit,
       }
     }
+    console.log(inventoryItemData);
     console.log("Inventory item initialized!");
-    console.log(inventoryItem);
     try {
       const response = await fetch("/inventoryitem/create", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(inventoryItem)
+        body: JSON.stringify(inventoryItemData)
       });
-      setData("Post request successful!");
       console.log("Request successful!");
+      navigate("/inventory/list");
     } catch (error) {
       console.log(error);
     }
     console.log("End of function.");
   }
 
+  function handleInputChange(event) {
+    const inputName = event.target.name;
+    setInventoryItem((inventoryItem) => {
+      return {
+        ...inventoryItem,
+        [`${inputName}`]: event.target.value,
+      }
+    })
+  }
+
   return(
     <div>
-      <p>Here's the inventory item form!</p>
-      <button onClick={addInventoryItem}>Add Item</button>
-      <p>{data}</p>
+      <h1>Create inventory item</h1>
+      <p>When you create an inventory item, you can then add it to a recipe.</p>  
+      <p>Adding an inventory item to a recipe lets Bento deduct a preset amount
+        of that item from inventory whenever the user buys 
+        a specific catalog item ("makes the recipe").
+      </p>  
+      <p>Example:</p>
+      <ol>
+        <li>You create an inventory item called Waffle Cones.</li>
+        <li>You set up a recipe for Ice Cream Surprise, which is
+          an existing item in your Square catalog. In the recipe,
+          you specify that 1 Waffle Cone is needed for each 
+          order of Ice Cream Surprise.
+        </li>
+        <li>When a customer orders Ice Cream Surprise, Bento will
+          automatically deduct 1 Waffle Cone from your inventory!
+        </li>
+      </ol>
+      <form>
+        <TextField 
+          name="name"
+          label="Item name"
+          onChange={handleInputChange}
+          value={inventoryItem.name}
+          variant="outlined"
+        />
+        <TextField 
+          name="quantity_in_stock"
+          label="Quantity in stock"
+          onChange={handleInputChange}
+          type="number"
+          value={inventoryItem.quantity_in_stock}
+          variant="outlined"
+        />     
+        <TextField 
+          name="singular_unit"
+          label="Singular unit"
+          onChange={handleInputChange}
+          value={inventoryItem.singular_unit}
+          variant="outlined"
+        />            
+        <TextField 
+          name="plural_unit"
+          label="Plural unit"
+          onChange={handleInputChange}
+          value={inventoryItem.plural_unit}
+          variant="outlined"
+        />  
+        <Button onClick={createInventoryItem} type="submit" variant="outlined">Create Item</Button>
+      </form>
     </div>
   );
 }
