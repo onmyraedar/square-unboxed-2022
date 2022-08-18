f7c9d144233293a55ede6d48a74f0d2635245240
Reanna Ishmael <rishmael@fordham.edu>
2022-08-18 15:45:48 -0400

    Update Order schema
    When a customer completes the checkout process, we want to save their order to our database.
    
    Add fields to the Order schema to save necessary data about line items.

--- models/order.js
+++ models/order.js
@@ -3,8 +3,24 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const OrderSchema = new Schema({
-    terminal_event_type: { type: String, required: true },
-    terminal_event_id: { type: String, required: true },
+    order_id: { type: String, required: true },
+    line_items: [{
+        variation_catalog_object_id: { type: String, required: true },
+        name: { type: String, required: true },
+        variation_name: { type: String, required: true },
+        quantity: { type: Schema.Types.Decimal128 },
+
+        // Modifiers are not required: an item may not have any
+        modifiers: [{
+            catalog_object_id: { type: String },
+            name: { type: String },
+            quantity: { type: Schema.Types.Decimal128 },
+        }],
+
+        // Changes are not required: if an item has no recipe in the database,
+        // there will be no need for a change
+        inventory_item_changes: [{ type: Schema.Types.ObjectId, ref: "InventoryItemChange" }]
+    }]
 });
 
 module.exports = mongoose.model("Order", OrderSchema);
